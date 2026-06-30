48f9caa2f4099e864d9b49d479971393203c396b
jjliaomail-wq <jjliao.mail@gmail.com>
2026-07-01 01:23:05 +0800

    Auto Update

---
+++ update.bat
@@ -0,0 +1,23 @@
+@echo off
+echo ==================================================
+echo   TOMICA Price Tracker - Auto Update Script
+echo ==================================================
+echo.
+
+echo [1/3] Scanning for changed files...
+git add .
+echo.
+
+echo [2/3] Creating commit...
+git commit -m "Auto Update"
+echo.
+
+echo [3/3] Pushing to remote repository (GitHub)...
+git push origin main
+git push origin master
+
+echo.
+echo ==================================================
+echo   Done! Check above for any errors.
+echo ==================================================
+pause
